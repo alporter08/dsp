90faa2402a2700bef309a1f22c9ea25267a1e7f3
AP <adrienporter@Adriens-MacBook-Pro.local>
2016-11-23 13:25:14 -0500

    added q5 part a)

--- python/q5_datetime.py
+++ python/q5_datetime.py
@@ -1,13 +1,21 @@
 # Hint:  use Google to find python function
 
-####a) 
-date_start = '01-02-2013'  
-date_stop = '07-28-2015'   
+####a)
+import datetime
 
-####b)  
-date_start = '12312013'  
-date_stop = '05282015'  
+date_start = '01-02-2013'
+date_stop = '07-28-2015'
 
-####c)  
-date_start = '15-Jan-1994'  
-date_stop = '14-Jul-2015'  
+a = datetime.datetime.strptime(date_start, "%m-%d-%Y")
+b = datetime.datetime.strptime(date_stop, "%m-%d-%Y")
+c = b - a
+print str(c)
+
+
+####b)
+date_start = '12312013'
+date_stop = '05282015'
+
+####c)
+date_start = '15-Jan-1994'
+date_stop = '14-Jul-2015'
