2e71155d23433b9ab74056a1d12607945f9a5687
Adrien Porter <adrien.porter@gmail.com>
2016-11-25 18:07:16 -0500

    added q6 strings both_ends

--- python/q6_strings.py
+++ python/q6_strings.py
@@ -45,7 +45,14 @@ def both_ends(s):
     >>> both_ends('xyz')
     'xyyz'
     """
-    raise NotImplementedError
+
+    if len(s) < 2:
+        return ''
+
+    else:
+        return s[:2] + s[len(s)-2:]
+
+
 
 
 def fix_start(s):
