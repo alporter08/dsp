3a439d8c96dcfdc0c90a4332ee5ceb0fc77949f2
Adrien Porter <adrien.porter@gmail.com>
2016-11-25 17:49:10 -0500

    added q5 part a)

--- python/q5_datetime.py
+++ python/q5_datetime.py
@@ -16,6 +16,16 @@ print str(c)
 date_start = '12312013'
 date_stop = '05282015'
 
+a = datetime.datetime.strptime(date_start, "%m%d%Y")
+b = datetime.datetime.strptime(date_stop, "%m%d%Y")
+c = b - a
+print str(c)
+
 ####c)
 date_start = '15-Jan-1994'
 date_stop = '14-Jul-2015'
+
+a = datetime.datetime.strptime(date_start, "%d-%b-%Y")
+b = datetime.datetime.strptime(date_stop, "%d-%b-%Y")
+c = b - a
+print str(c)
