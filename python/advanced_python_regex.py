23f48b3540aa0213a428563728ca5032c88b2832
Adrien Porter <adrien.porter@gmail.com>
2016-11-28 15:05:00 -0500

    answered q1 and q2

--- python/advanced_python_regex.py
+++ python/advanced_python_regex.py
@@ -1,1 +1,56 @@
-PLACE YOUR CODE HERE
+import re
+
+def degrees(file):
+    fin = open(file)
+    next(fin) #skip first line (header)
+
+    degrees = []
+
+    for line in fin:
+        l = line.split(',')
+        l = [x.strip() for x in l]
+        degrees.append(l[1])
+
+    degrees_clean = []
+
+    for item in degrees:
+        x = re.sub('\.','',item) #remove periods from degrees
+        degrees_clean = degrees_clean + x.split()
+
+#create dictionary with frequencies
+    d = {}
+
+    for item in degrees_clean:
+        if item not in d:
+            d[item] = 1
+        else:
+            d[item] += 1
+
+
+    return len(d), d
+
+
+def titles(file):
+    fin = open(file)
+    next(fin) #skip first line (header)
+
+    titles = []
+
+    for line in fin:
+        l = line.split(',')
+        l = [x.strip() for x in l]
+        title = l[2]
+        x = re.sub(' .. Biostatistics','',title) #remove ' of Biostatistics' from degrees
+        titles.append(x)
+
+#create dictionary with frequencies
+    d = {}
+
+    for item in titles:
+        if item not in d:
+            d[item] = 1
+        else:
+            d[item] += 1
+
+
+    return len(d), d
