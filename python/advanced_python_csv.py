70b5b3e3ae177e19bdc1342f664f5ed9c5bb1bfd
Adrien Porter <adrien.porter@gmail.com>
2016-11-30 11:56:23 -0500

    added advanced_python_csv and emails.csv

--- python/advanced_python_csv.py
+++ python/advanced_python_csv.py
@@ -1,1 +1,21 @@
-PLACE YOUR CODE HERE
+# -*- coding: utf-8 -*-
+
+def emails(file):
+    fin = open(file)
+    next(fin) #skip first line (header)
+
+    emails = []
+
+    for line in fin:
+        l = line.split(',')
+        l = [x.strip() for x in l]
+        emails.append(l[3])
+
+    fout(emails, 'emails.csv')
+
+def fout(output, file):
+    fout = open(file,'w')
+    for item in output:
+        fout.write(item)
+        fout.write('\n')
+    fout.close()
