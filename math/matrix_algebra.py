c24232ab92a83de232f447dd1aa64d2b8883ce4b
Adrien <adrien.porter@gmail.com>
2016-12-13 19:00:35 -0500

    Update matrix_algebra.py

--- math/matrix_algebra.py
+++ math/matrix_algebra.py
@@ -45,23 +45,31 @@ try:
     print 'A + C = ', A + C
 except:
     print 'A + C is not defined'
+ # A + C =  A + C is not defined
 
 try:
     print 'A - C^T = ', A - np.transpose(C)
 except:
     print 'A - C^T is not defined'
+ # A - C^T =  [[-4 -7 -3]
+ #             [ 3  6  4]]
 
 try:
     print 'C^T + 3 * D = ', np.transpose(C) + 3 * D
 except:
     print 'C^T + 3D is not defined'
+ # C^T + 3 * D =  [[14  3  3]
+ #                 [ 2  7  9]]
 
 try:
     print 'BA = ', B.dot(A)
 except:
     print 'BA is not defined'
+ # BA =  [[-1 -5 -1]
+ #        [ 2  7  4]]
 
 try:
     print 'BA^T = ', B.dot(np.transpose(A))
 except:
     print 'BA^T is not defined'
+ # BA^T =  BA^T is not defined
